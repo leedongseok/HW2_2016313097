b0ceae6c40c9a8c678d8d88f85b26940505506d6
leedongseok <ehdtjr6682@naver.com>
2022-09-05 23:47:19 +0900

    modifiy count.cc

---
+++ count.cc
@@ -0,0 +1,14 @@
+#include <iostream>
+#include "count.h"
+
+using namespace std;
+
+void count_(int num){
+	int a = 0;
+	while (a<num){
+		cout << a << endl;
+		++a;
+	}
+	cout << "Start!!" << endl;
+	cout << "2016313097 이동석" << endl;
+}
